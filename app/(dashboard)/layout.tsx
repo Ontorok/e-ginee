79bf4662a3785bae5a15b1be5fea75dbed999236
Nasir Ahmed <nasir.ahmed@bizzntek.com>
2023-08-24 13:07:43 +0600

    feat: add mobile sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,10 @@ type Props = {};
 const DashboardLayout = ({ children }: Props & PropsWithChildren) => {
   return (
     <div className="h-full relative">
-      <Sidebar />
+      <div className="hidden h-full md:w-72 md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+        <Sidebar />
+      </div>
+
       <main className="md:pl-72">
         <Navbar />
         {children}
